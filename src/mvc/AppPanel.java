19b833f67789461c9777265af6097d3199ff7870
Ziwei Fang <fzw8833@yahoo.com>
2024-03-14 14:46:24 -0700

    Added a class

--- src/mvc/AppPanel.java
+++ src/mvc/AppPanel.java
@@ -1,72 +1,98 @@
 package mvc;
 
-import StoplightSimulator.StoplightFactory;
-
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class AppPanel extends JPanel implements Subscriber {
+// rename as Controller?
+public class AppPanel extends JPanel implements Subscriber, ActionListener  {
+
+    protected Model model;
     protected AppFactory factory;
-    private Publisher publisher;
-    private View view;
-
-    // Nested ControlPanel class
-    public class ControlPanel extends JPanel implements ActionListener {
-        private JButton changeButton;
-
-        public ControlPanel() {
-            changeButton = new JButton("Change");
-            changeButton.addActionListener(this);
-            add(changeButton);
-        }
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            if (e.getSource() == changeButton) {
-                Command command = factory.makeEditCommand(null, "Change", this);
-                if (command != null) {
-                    command.execute();
-                }
-            }
-        }
-    }
+    protected View view;
+    protected JPanel controlPanel;
+    private JFrame frame;
+    public static int FRAME_WIDTH = 500;
+    public static int FRAME_HEIGHT = 300;
 
     public AppPanel(AppFactory factory) {
-        this.factory = factory;
-        this.publisher = new Publisher();
-        initComponents();
+
+        // initialize fields here
+
+        frame = new SafeFrame();
+        Container cp = frame.getContentPane();
+        cp.add(this);
+        frame.setJMenuBar(createMenuBar());
+        frame.setTitle(factory.getTitle());
+        frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
     }
 
-    private void initComponents() {
-        ControlPanel controlPanel = new ControlPanel();
-        add(controlPanel);
+    public void display() { frame.setVisible(true); }
 
-        // Create and add the view (stoplight) to the panel
-        Model model = factory.makeModel();
-        view = factory.makeView(model);
-        publisher.addSubscriber(view);
-        add((Component) view);
+    public void update() {  /* override in extensions if needed */ }
+
+    public Model getModel() { return model; }
+
+    // called by file/open and file/new
+    public void setModel(Model newModel) {
+        this.model.unsubscribe(this);
+        this.model = newModel;
+        this.model.subscribe(this);
+        // view must also unsubscribe then resubscribe:
+        view.setModel(this.model);
+        model.changed();
     }
 
-    public Publisher getPublisher() {
-        return publisher;
+    protected JMenuBar createMenuBar() {
+        JMenuBar result = new JMenuBar();
+        // add file, edit, and help menus
+        JMenu fileMenu =
+                Utilities.makeMenu("File", new String[] {"New",  "Save", "SaveAs", "Open", "Quit"}, this);
+        result.add(fileMenu);
+
+        JMenu editMenu =
+                Utilities.makeMenu("Edit", factory.getEditCommands(), this);
+        result.add(editMenu);
+
+        JMenu helpMenu =
+                Utilities.makeMenu("Help", new String[] {"About", "Help"}, this);
+        result.add(helpMenu);
+
+        return result;
     }
 
-    @Override
-    public void update() {
-        // Implement update logic as needed
+    public void actionPerformed(ActionEvent ae) {
+        try {
+            String cmmd = ae.getActionCommand();
+
+            if (cmmd.equals("Save")) {
+                Utilities.save(model, false);
+            } else if (cmmd.equals("SaveAs")) {
+                Utilities.save(model, true);
+            } else if (cmmd.equals("Open")) {
+                Model newModel = Utilities.open(model);
+                if (newModel != null) setModel(newModel);
+            } else if (cmmd.equals("New")) {
+                Utilities.saveChanges(model);
+                setModel(factory.makeModel());
+                // needed cuz setModel sets to true:
+                model.setUnsavedChanges(false);
+            } else if (cmmd.equals("Quit")) {
+                Utilities.saveChanges(model);
+                System.exit(0);
+            } else if (cmmd.equals("About")) {
+                Utilities.inform(factory.about());
+            } else if (cmmd.equals("Help")) {
+                Utilities.inform(factory.getHelp());
+            } else { // must be from Edit menu
+                //???
+            }
+        } catch (Exception e) {
+            handleException(e);
+        }
     }
 
-    public static void main(String[] args) {
-        // For testing purpose
-        JFrame frame = new JFrame("MVC App");
-        AppFactory factory = new StoplightFactory();
-        AppPanel panel = new AppPanel(factory);
-        frame.getContentPane().add(panel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
-        frame.setVisible(true);
+    protected void handleException(Exception e) {
+        Utilities.error(e);
     }
 }
