17c95d248845b12e2ae86a52b8d0df0e6595ac53
Ziwei Fang <fzw8833@yahoo.com>
2024-03-20 08:25:31 -0700

    Added a class

--- src/LifeLab/Agent.java
+++ src/LifeLab/Agent.java
@@ -1,4 +1,35 @@
 package LifeLab;
 
-public class Agent {
+import CALab.Cell;
+import CALab.Grid;
+
+public class Agent extends Cell {
+    public Agent(int row, int col, Grid grid) {
+        super(row, col, grid);
+    }
+
+    @Override
+    public void observe() {
+
+    }
+
+    @Override
+    public void interact() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void nextState() {
+
+    }
+
+    @Override
+    public void reset(boolean randomly) {
+
+    }
 }
--- src/LifeLab/Society.java
+++ src/LifeLab/Society.java
@@ -1,4 +1,23 @@
 package LifeLab;
 
-public class Society {
+
+import CALab.Grid;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Society extends Grid {
+
+    // Factory method to create instances of Cell
+
+
+    public static Set<Integer> rebirth = new HashSet<Integer>();
+    public static Set<Integer> death = new HashSet<Integer>();
+    public static int percentAlive = 50;
+
+    @Override
+    public Agent makeCell(boolean uniform) {
+        // You can use the factory method here if needed
+        return null;
+    }
 }
